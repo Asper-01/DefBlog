a51ae4304de6eade6a6a6fafd477eacc307dc553
Francois Pazery <asper-01@hotmail.fr>
2024-11-18 11:47:26 +0100

    routes redirections et views

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -2,6 +2,7 @@
 module Admin
   class ArticlesController < ApplicationController
     before_action :authenticate_user!  # Si tu utilises Devise
+    before_action :set_article, only: [:edit, :update, :destroy]
 
     def index
       @articles = Article.all
@@ -19,11 +20,37 @@ module Admin
         render :new
       end
     end
+    def edit
+      @article = Article.find(params[:id]) # Cette ligne doit être présente
+    end
+
+    def update
+      if @article.update(article_params)
+        redirect_to admin_articles_path, notice: 'Article mis à jour avec succès.'
+      else
+        render :edit
+      end
+    end
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      redirect_to admin_articles_path, notice: 'Article supprimé avec succès.'
+    end
 
     private
 
     def article_params
       params.require(:article).permit(:title, :content)  # adapte selon tes attributs d'article
     end
+    def find_article
+      @article = Article.find(params[:id]) # Recherche l'article par son ID
+    end
+    def set_article
+      Rails.logger.debug "Params ID: #{params[:id]}"
+      @article = Article.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to admin_articles_path, alert: "Article introuvable."
+    end
+
   end
 end
