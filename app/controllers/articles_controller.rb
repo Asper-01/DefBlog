9dc22eab7e8b02250960bc86d567bde17a99deef
Francois Pazery <asper-01@hotmail.fr>
2024-11-20 11:39:46 +0100

    show acces fix

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!, except: [:index]  # Permet l'accès à index sans être connecté
+  before_action :authenticate_user!, except: [:index, :show ]  # Permet l'accès à index sans être connecté
   before_action :require_admin, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_article, only: %i[show edit update destroy]
 
