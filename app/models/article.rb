32777e689e8aa51a9a4ed1ef6291cb09dc78e7cd
Francois Pazery <asper-01@hotmail.fr>
2024-11-29 20:48:52 +0100

    modifs front

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   # Associations
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: "author_id"
   has_one_attached :image
   has_and_belongs_to_many :tags, join_table: :articletags
 
